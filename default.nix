f06ac5f09f6ca9b4586bdd19ebf526322b84cda2
rexim <reximkut@gmail.com>
2017-11-04 02:49:31 +0700

    Add openblas to LD_LIBRARY_PATH (#1)

--- default.nix
+++ default.nix
@@ -2,5 +2,6 @@ with import <nixpkgs> {}; {
     boidEnv = stdenv.mkDerivation {
         name = "boid-env";
         buildInputs = [ ghc cabal-install stack openblas ];
+        LD_LIBRARY_PATH="${openblas}/lib";
     };
 }
