44498764d2a6f57bc422a810e22dc0324d671616
rexim <reximkut@gmail.com>
2017-11-04 18:56:12 +0700

    Disable blas64 for openblas package (#8)

--- default.nix
+++ default.nix
@@ -1,7 +1,10 @@
-with import <nixpkgs> {}; {
-    boidEnv = stdenv.mkDerivation {
-        name = "boid-env";
-        buildInputs = [ ghc cabal-install stack openblas ];
-        LD_LIBRARY_PATH="${openblas}/lib";
-    };
+let
+  nixos = import <nixpkgs> {};
+  fixedOpenblas = nixos.openblas.override { blas64 = false; };
+in rec {
+  nnaskellEnv = nixos.stdenv.mkDerivation {
+      name = "nnaskell-env";
+      buildInputs = [ nixos.ghc nixos.cabal-install nixos.stack fixedOpenblas ];
+      LD_LIBRARY_PATH="${fixedOpenblas}/lib";
+  };
 }
