f6d19a4b3c793a8fcdaa43dd01de6df3dced324b
rexim <reximkut@gmail.com>
2017-11-11 23:27:32 +0700

    Add Xor module to the build

--- src/Xor.hs
+++ src/Xor.hs
@@ -4,7 +4,7 @@ import Nnaskell
 import Numeric.LinearAlgebra.Data
 
 xorNN = do nn <- randomNN [2, 2, 1]
-            return $ head $ drop 5000 $ optimizeCost (cost xorTD) nn
+           return $ head $ drop 5000 $ optimizeCost (cost xorTD) nn
 
 xorTD :: [(Vector R, Vector R)]
 xorTD = [ (vector [0.0, 0.0], vector [0.0])
