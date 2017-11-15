7e8400647f8f7d093065db1cd358d871fdf0a548
rexim <reximkut@gmail.com>
2017-11-16 04:48:34 +0700

    Implement readLabelsFile (#5)

--- src/MNIST.hs
+++ src/MNIST.hs
@@ -7,22 +7,36 @@ import qualified Data.ByteString.Lazy as BL
 
 type Image = [Int]
 
-list2Number :: BL.ByteString -> Int
-list2Number = foldl (\a x -> a * 0xff + x) 0 . map fromIntegral . BL.unpack
+bytesAsInt :: BL.ByteString -> Int
+bytesAsInt = foldl (\a x -> a * 0x100 + x) 0 . map fromIntegral . BL.unpack
 
-labelsMagicNumberParser :: BL.ByteString -> IO BL.ByteString
-labelsMagicNumberParser bs = let magicNumber = list2Number $ BL.take 4 bs
-                                 body = BL.drop 4 bs
-                             in if magicNumber == 2049
-                                then return body
-                                else ioError $ userError $ printf "Unexpected magic number: %d" magicNumber
+parseInt32 :: BL.ByteString -> (Int, BL.ByteString)
+parseInt32 bs = (bytesAsInt $ BL.take 4 bs, BL.drop 4 bs)
 
-labelsDataParser :: BL.ByteString -> IO [Int]
-labelsDataParser bs = undefined
+magicNumberParser :: Int -> BL.ByteString -> IO BL.ByteString
+magicNumberParser expectedNumber bs
+    | expectedNumber == actualNumber = return rest
+    | otherwise = ioError
+                  $ userError
+                  $ printf "Unexpected magic number: %d. Expected %d." actualNumber expectedNumber
+    where (actualNumber, rest) = parseInt32 bs
+
+labelsParser :: BL.ByteString -> IO [Int]
+labelsParser bs =
+    let (expectedCount, body) = parseInt32 bs
+        labels = map fromIntegral $ BL.unpack body
+        actualCount = length labels
+    in if expectedCount == actualCount
+       then return labels
+       else ioError
+            $ userError
+            $ printf "Unexpected amount of labels. Expected: %d, but got %d" expectedCount actualCount
 
 readLabelsFile :: FilePath -> IO [Int]
 readLabelsFile fileName =
-    BL.readFile fileName >>= labelsMagicNumberParser >>= labelsDataParser
+    BL.readFile fileName
+    >>= magicNumberParser 2049
+    >>= labelsParser
 
 readImagesFile :: FilePath -> IO [Image]
 readImagesFile = undefined
